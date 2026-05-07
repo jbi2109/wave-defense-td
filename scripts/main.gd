e5e9efb1038b58c929ca55af0ed677a29cb11ea2
Joshua Irvine <jbijoshua@gmail.com>
2026-05-07 20:53:14 +0100

    feat: implement TileMapDual for rounded map corners

--- scripts/main.gd
+++ scripts/main.gd
@@ -28,46 +28,84 @@ func _ready():
 	$HUD/Overlay/GameOverContainer/RestartButton.pressed.connect(_on_restart_button_pressed)
 
 func _setup_map_visuals():
-	grass_layer.tile_set = TileSet.new()
-	grass_layer.tile_set.tile_size = Vector2i(32, 32)
+	# 1. SETUP TILEMAPDUAL TILESET (Visuals)
+	var ts = TileSet.new()
+	ts.tile_size = Vector2i(32, 32)
 	
-	# Grass gets collision layer 1
-	grass_layer.tile_set.add_physics_layer(0)
-	grass_layer.tile_set.set_physics_layer_collision_layer(0, 1)
-	grass_layer.tile_set.set_physics_layer_collision_mask(0, 1)
+	ts.add_terrain_set(0)
+	ts.set_terrain_set_mode(0, TileSet.TERRAIN_MODE_MATCH_CORNERS)
+	ts.add_terrain(0, 0) # Terrain 0: BG (Grass/Cliffs)
+	ts.add_terrain(0, 1) # Terrain 1: FG (Dirt Path)
 	
-	dirt_layer.tile_set = TileSet.new()
-	dirt_layer.tile_set.tile_size = Vector2i(32, 32)
+	var dual_tex = load("res://assets/shader/shader-tileset.png")
+	var source = TileSetAtlasSource.new()
+	source.texture = dual_tex
+	source.texture_region_size = Vector2i(16, 16) # Dual grid uses half-size tiles
+	
+	# The 16 preset tiles required by TileMapDual topology SQUARE Standard
+	var preset_tiles = [
+		Vector2i(0, 3), Vector2i(3, 3), Vector2i(0, 2), Vector2i(1, 2),
+		Vector2i(0, 0), Vector2i(3, 2), Vector2i(2, 3), Vector2i(3, 1),
+		Vector2i(1, 3), Vector2i(0, 1), Vector2i(1, 0), Vector2i(2, 2),
+		Vector2i(3, 0), Vector2i(2, 0), Vector2i(1, 1), Vector2i(2, 1)
+	]
+	
+	for i in range(16):
+		var atlas_pos = preset_tiles[i]
+		source.create_tile(atlas_pos)
+		var td = source.get_tile_data(atlas_pos, 0)
+		td.terrain_set = 0
+		td.terrain = -1
+		
+		# Set peering bits (0 is bg, 1 is fg)
+		var tl = 1 if (i & 1) else 0
+		var tr = 1 if (i & 2) else 0
+		var bl = 1 if (i & 4) else 0
+		var br = 1 if (i & 8) else 0
+		
+		# In Match Corners and Sides, we set the corners
+		td.set_terrain_peering_bit(TileSet.CELL_NEIGHBOR_TOP_LEFT_CORNER, tl)
+		td.set_terrain_peering_bit(TileSet.CELL_NEIGHBOR_TOP_RIGHT_CORNER, tr)
+		td.set_terrain_peering_bit(TileSet.CELL_NEIGHBOR_BOTTOM_LEFT_CORNER, bl)
+		td.set_terrain_peering_bit(TileSet.CELL_NEIGHBOR_BOTTOM_RIGHT_CORNER, br)
 
-	var grass_tex = load("res://assets/shader/Shader_cliff.png")
-	var dirt_tex = load("res://assets/shader/shader-texture_1.png")
+	ts.add_source(source, 0)
 	
-	var source_grass = TileSetAtlasSource.new()
-	source_grass.texture = grass_tex
-	source_grass.texture_region_size = Vector2i(32, 32)
-	source_grass.create_tile(Vector2i(0, 0))
-	# Add physics polygon to grass
-	var tile_data = source_grass.get_tile_data(Vector2i(0,0), 0)
-	tile_data.add_collision_polygon(0)
-	var polygon = PackedVector2Array([Vector2(-16, -16), Vector2(16, -16), Vector2(16, 16), Vector2(-16, 16)])
-	tile_data.set_collision_polygon_points(0, 0, polygon)
+	dirt_layer.tile_set = ts
 	
-	grass_layer.tile_set.add_source(source_grass, 0)
+	# Tell TileMapDual to manually initialize
+	if dirt_layer.has_method("_changed"):
+		dirt_layer._changed()
+		
+	# 2. SETUP PHYSICS TILESET (Invisible layer just for collisions)
+	var physics_ts = TileSet.new()
+	physics_ts.tile_size = Vector2i(32, 32)
+	physics_ts.add_physics_layer(0)
+	physics_ts.set_physics_layer_collision_layer(0, 1)
+	physics_ts.set_physics_layer_collision_mask(0, 1)
 	
-	var source_dirt = TileSetAtlasSource.new()
-	source_dirt.texture = dirt_tex
-	source_dirt.texture_region_size = Vector2i(32, 32)
-	source_dirt.create_tile(Vector2i(0, 0))
-	dirt_layer.tile_set.add_source(source_dirt, 0)
-
-	# 1. Fill EVERYTHING with Grass (Walls)
+	var phys_source = TileSetAtlasSource.new()
+	phys_source.texture = load("res://assets/shader/Shader_cliff.png")
+	phys_source.texture_region_size = Vector2i(32, 32)
+	phys_source.create_tile(Vector2i(0, 0))
+	var ptd = phys_source.get_tile_data(Vector2i(0,0), 0)
+	ptd.add_collision_polygon(0)
+	ptd.set_collision_polygon_points(0, 0, PackedVector2Array([Vector2(-16, -16), Vector2(16, -16), Vector2(16, 16), Vector2(-16, 16)]))
+	physics_ts.add_source(phys_source, 0)
+	
+	grass_layer.tile_set = physics_ts
+	grass_layer.visible = false # Hide it so we only see TileMapDual
+	
+	# 3. DRAW MAP
+	# Fill EVERYTHING with Grass (Walls)
 	grass_layer.clear()
 	for x in range(-5, 65):
 		for y in range(-5, 40):
 			grass_layer.set_cell(Vector2i(x, y), 0, Vector2i(0, 0))
+			if dirt_layer.has_method("draw_cell"):
+				dirt_layer.draw_cell(Vector2i(x, y), 0)
 	
-	# 2. Draw Complex Dirt Path
-	dirt_layer.clear()
+	# Draw Complex Dirt Path
 	var path_cells = []
 	for x in range(-5, 22):
 		for y in range(4, 9): path_cells.append(Vector2i(x, y))
@@ -89,9 +127,9 @@ func _setup_map_visuals():
 		for y in range(20, 25): path_cells.append(Vector2i(x, y))
 
 	for cell in path_cells:
-		dirt_layer.set_cell(cell, 0, Vector2i(0, 0))
-		# Remove Grass underneath
-		grass_layer.set_cell(cell, -1, Vector2i(-1, -1))
+		grass_layer.set_cell(cell, -1, Vector2i(-1, -1)) # Remove collision
+		if dirt_layer.has_method("draw_cell"):
+			dirt_layer.draw_cell(cell, 1) # Draw Dirt
 		
 	# Update Flow Field Obstacles based on Path
 	for x in range(-5, 80):
@@ -134,7 +172,7 @@ func _on_nexus_destroyed():
 
 func _start_next_wave():
 	current_wave += 1
-	enemies_to_spawn = 100 + (current_wave * 50) 
+	enemies_to_spawn = 150 + (current_wave - 1) * 50 
 	enemies_spawned = 0
 	is_spawning = true
 	spawn_timer = 0
